71b5d04b30bce6ba27fc83dd525e35f9cb4df92e
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2016-08-28 19:29:54 -0600

    Prepare gh-pages branch for distribution

--- src/alfredo/input/KeyDownEvent.java
+++
@@ -1,9 +0,0 @@
-package alfredo.input;
-
-/**
- * A KeyDownEvent is called when a key is pressed.
- * @author TheMonsterFromTheDeep
- */
-public interface KeyDownEvent {
-    void keyDown(int key);
-}
--- src/alfredo/input/KeyUpEvent.java
+++
@@ -1,9 +0,0 @@
-package alfredo.input;
-
-/**
- * A KeyUpEvent is called when a key is released.
- * @author TheMonsterFromTheDeep
- */
-public interface KeyUpEvent {
-    void keyUp(int key);
-}
